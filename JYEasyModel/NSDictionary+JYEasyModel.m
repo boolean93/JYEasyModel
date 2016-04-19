c2c875d2bf405661c0d0db7d0e6c8281dc375a49
BooleanMac <boolean93@163.com>
2016-04-20 01:14:31 +0800

    加入对类型是否可match的方法,但还未完善.

--- JYEasyModel/NSDictionary+JYEasyModel.m
+++ JYEasyModel/NSDictionary+JYEasyModel.m
@@ -15,7 +15,18 @@
     id matchedValue = [self valueForKey:property.propertyName];
     JYTypeEncoding valueType = [JYPropertyMeta getTypeEncodingForClass:object_getClass(matchedValue)];
     JYTypeEncoding propertyType = property.type;
-    return matchedValue;
+    if ([self.class canMatchFrom:valueType to:propertyType]) {
+        return matchedValue;
+    }
+    return nil;
+}
+
+// TODO: 不同类型的对象之间的判断
++ (BOOL)canMatchFrom:(JYTypeEncoding)fromType to:(JYTypeEncoding)toType {
+    if (fromType == toType) {
+        return YES;
+    }
+    return NO;
 }
 
 @end
